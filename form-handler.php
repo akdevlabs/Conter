f8f1abf0e3b3301a892176cd12d60c4e41796bd3
akdevlabs <116324447+akdevlabs@users.noreply.github.com>
2023-03-28 22:46:15 -0600

    Delete form-handler.php

--- form-handler.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$Date =  $_Post['date'];
-$Phone =  $_Post['phone'];
-$choose = $_Post['choose'];
-$message =  $_Post['message'];
-?>
